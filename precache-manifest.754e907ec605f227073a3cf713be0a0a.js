fa39826e986f1018fe3883edca233ebd90b8f23e
Manstrike <onpu4536b@gmail.com>
2020-08-11 00:11:28 +0300

    Updates

---
+++ precache-manifest.754e907ec605f227073a3cf713be0a0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bbd4633b7ad96ab50675a65f441f9b1",
+    "url": "/db-entry-frontend/index.html"
+  },
+  {
+    "revision": "a0bf1a6005603d523097",
+    "url": "/db-entry-frontend/static/css/main.dbf0a429.chunk.css"
+  },
+  {
+    "revision": "3406bab6fdc65c2905a1",
+    "url": "/db-entry-frontend/static/js/2.49c88bdb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/db-entry-frontend/static/js/2.49c88bdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0bf1a6005603d523097",
+    "url": "/db-entry-frontend/static/js/main.52427558.chunk.js"
+  },
+  {
+    "revision": "c6f40ac8c4d8f7ba7591",
+    "url": "/db-entry-frontend/static/js/runtime-main.d0283fca.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/db-entry-frontend/precache-manifest.754e907ec605f227073a3cf713be0a0a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/db-entry-frontend/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
